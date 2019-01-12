a61fb4f06c830690f1e26219778c4f29a4f0873e
Nick Inman <nick@theoatmeal.com>
2019-01-12 19:32:29 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,9 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.each {|key,value| "#{key} ="}}"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.each do |key,value| "#{key} ="
+
+  end }"
     DB[:conn].execute(sql)
 
   end
